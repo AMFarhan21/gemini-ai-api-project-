9ad3aeeb43a8d5f2dd07d5913398fbf3c43991d4
AMFarhan21 <AMFarhan2102@gmail.com>
2025-08-01 23:33:49 +0800

    change the endpoint

--- public/script.js
+++ public/script.js
@@ -27,9 +27,11 @@ form.addEventListener('submit', async function (e) {
   // 2. Show a temporary "Thinking..." message and store the element
   const botPlaceholder = appendMessage('bot', 'Thinking...');
 
+  console.log("PRODUCTION ENDPOINT: ", process.env.PRODUCTION_ENDPOINT)
+
   try {
     // 3. Send the user's message to the backend
-    const response = await fetch(`${process.env.PRODUCTION_ENDPOINT}/chat`, {
+    const response = await fetch("https://gemini-ai-api-project.onrender.com/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
